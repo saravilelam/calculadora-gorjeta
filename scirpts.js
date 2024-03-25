d02443edb9eeadd723f47da1ca904e269515b6ee
Sara vilela <sara.bizerra@icloud.com>
2024-03-25 20:48:52 -0300

    Segundo commit do JS

--- scirpts.js
+++ scirpts.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 let contaInput = document.querySelector ("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -12,11 +13,39 @@ const pessoasInpnut = document.querySelector("#pessoas")
 pessoasInpnut.addEventListener ("input", receberQuantidadePessoas)
 
 function receberQuantidadePessoas(evento) {
-   if(evento.target.value==="0") {
+
     const paragrafoErro = document.querySelector(".pessoas #erro").display = "block"
-    paragrafoErro.style.display = "block"
 
     const divErro = document.querySelector(".pessoas .input-box")
+
+   if(evento.target.value==="0") {
+    
+    paragrafoErro.style.display = "block"
+
     divErro.setAttribute ("id", "erro-div")
+   } else{
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.terget.value)
    }
+}
+
+const botoesGorjeta = document.querySelectorAllb(".gorjeta input[type='button'")
+botoesGorjeta.forEach(botao =>{
+    botao.addEventListener("click", receberPorcentagemBotao)
+})
+
+function receberPorcentagemBotao(evento) {
+
+    botoesGorjeta.forEach(botao =>{
+        botoesGorjeta.ClassList.remove("botao-ativo")
+        
+        if(botao.value===evento.target.value){
+            botao.ClassList.add("botao-ativo")
+        }
+    })
+    porcentagem = parseFloat(evento.target.value) /100
+
+
+
 }
